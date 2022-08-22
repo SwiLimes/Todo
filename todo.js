c5714fb18c7995533b49fda67dc6d06ae8823727
swilimes <nikitos15052001@gmail.com>
2022-08-22 17:46:58 +0300

    all functions, color-detect

--- todo.js
+++ todo.js
@@ -1,52 +1,71 @@
-const list = document.querySelector('#list');
-let idCounter = 0;
-
-let todo = [
-    {text: 'Пообедать', done: true, color: 'yellow'},
-    {text: 'Сделать практику', done: false},
-    {text: 'Написать программу', done: true, color: 'aqua'},
-];
-
-render(todo, list);
+const taskList = document.getElementById('list'),
+    controlList = document.querySelector('.functionBlock');
 
 
-/**
- * Добавление новой задачи в список
+let todo = [];
+if(localStorage.getItem('todo').length !=0 ) todo = JSON.parse(localStorage.getItem('todo'));
+
+
+let idIndex = 0;        //Счетчик для создания id у объектов-задач
+
+let selectedTask;
+let selectedIndex;
+
+
+/*
+    Смена классов при нажатии на объект
  */
-let addItem = function () {
-    let task = prompt('Введите название задачи:');
-    todo.push({text: task, done: false, color: 'orange'});
-    render(todo, list);
-};
+document.addEventListener('DOMContentLoaded', () => {
+    taskList.addEventListener('click', event => {
 
-let deleteItem = function () {
+        let li = event.target.closest('li');
 
-};
 
-/**
- * добавление в список дел из массива
- * @param todo - массив дел
- * @param elem - список в html
- */
-function render(todo, elem) {
-    // Очищается поле с задачами
-    let todoList = document.querySelectorAll('#taskBlock li');
-    todoList.forEach(task => {
-        task.remove();
+        if (!li) return;
+
+        if (selectedTask) {
+            selectedTask.classList.remove('active');
+            selectedTask.classList.add('noActive')
+        }
+
+        selectedTask = li;
+        selectedIndex = Number(li.id);
+        li.classList.add('active');
+        li.classList.remove('noActive');
+
+        let checkbox = li.lastElementChild;
+        if(checkbox.checked) {
+            todo[selectedIndex].done = true;
+        }
+        else todo[selectedIndex].done = false;
+        save();
     });
 
+    render(todo, taskList);
+})
+
+function render(todo, elem) {
+    // Очищается поле с задачами
+    taskList.replaceChildren();
+
     // Блок заполняется задачами из массива 'todo';
+    idIndex = 0;
     todo.forEach(task => {
-        const li = document.createElement('li');
-        li.setAttribute('id', 'task');
+
+        task.id = idIndex++;
+
+
+        let li = document.createElement('li');
+        li.setAttribute('id', `${task.id}`);
 
         const checkbox = document.createElement('input');
         checkbox.setAttribute('type', 'checkbox');
         checkbox.checked = task.done;
 
-        // if(task.color) {
-        //     li.style.background = task.color;
-        // }
+
+        if(task.hasOwnProperty('color')) {
+            li.style.backgroundColor = task.color;
+        }
 
 
         li.append(task.text);
@@ -54,47 +73,101 @@ function render(todo, elem) {
         li.classList.add('noActive');
 
         elem.append(li);
-        li.setAttribute('data-index', todo.indexOf(task))
     });
+
 };
 
-
-const elemm = document.querySelectorAll('#task');
-let current;
-
-function elemmClick() {
-    if(current && current !== this){
-        current.classList.toggle("noActive");
-        current.classList.toggle("active");
-    }
-
-    this.classList.toggle("noActive");
-    this.classList.toggle("active");
-    current = this;
+function save() {
+    localStorage.setItem('todo', JSON.stringify(todo));
 }
 
-for(let e of elemm) {
-    e.addEventListener("click", elemmClick);
-}
+// let changed = selectedTask.closest('li');
 
-const buttonDelete = document.getElementById('deleteTask');
-buttonDelete.addEventListener('click', () => {
-    if(current) {
-        let currentIndex = +current.dataset.index;
-        todo.splice(currentIndex, 1);
-        // for(let task of todo) {
-        //     if(current.dataset.index == todo.indexOf(task)) todo.splice(currentIndex, 0);
-        // }
-        current.remove();
+class Control {
+    constructor(option) {
+        this.option = option;
+        option.onclick = this.onClick.bind(this);
     }
 
-});
+    changeTask() {
+        if (!selectedTask) return;
 
-const buttonAdd = document.getElementById('addTask');
-buttonAdd.addEventListener('click', () => {
-    let task = prompt('Введите название задачи:');
-    let selectedIndex = +current.dataset.index;
-    todo.splice(selectedIndex, 0, {text: task, done: false, color: 'red'});
-    // todo.push({text: task, done: false, color: 'orange'});
-    render(todo, list);
-});
+        let task = todo[selectedIndex];
+        let newName = prompt("Введите изменения");
+        if (newName == null || newName == '') {
+            alert("Вы ничего не измении");
+            return;
+        }
+        task.text = newName;
+
+        let newColor = prompt('Новый цвет');
+        todo[selectedIndex].color = newColor;
+
+        selectedTask = null;
+
+        render(todo, taskList);
+    }
+
+    addTask() {
+        let taskName = prompt("Введите название задачи");
+        if (taskName == null || taskName == '') return;
+        let taskColor = prompt("Введице цвет задачи");
+        let task = {
+            text: taskName,
+            done: false,
+            color: taskColor
+        }
+
+        if (!selectedTask) todo.push(task);
+        else todo.splice(selectedIndex + 1, 0, task);
+    }
+
+    deleteTask() {
+        if (!selectedTask) return;
+
+        todo.splice(selectedIndex, 1);
+        selectedTask.remove();
+    }
+
+    upTask() {
+        if (!selectedTask) return;
+        if (selectedIndex == 0) return;
+
+        let tempVariable = todo[selectedIndex];
+        todo[selectedIndex] = todo[selectedIndex - 1];
+        todo[selectedIndex - 1] = tempVariable;
+    }
+
+    downTask() {
+        if (!selectedTask) return;
+        if (selectedIndex == todo.length - 1) return;
+
+        let tempVariable = todo[selectedIndex];
+        todo[selectedIndex] = todo[selectedIndex + 1];
+        todo[selectedIndex + 1] = tempVariable;
+    }
+
+
+    onClick(event) {
+        let optionId = event.target.id;
+
+        if (optionId) {
+            this[optionId]();
+            if(optionId != 'changeTask') render(todo, taskList);
+            else {
+
+            }
+            save();
+            selectedTask = null;
+        }
+    }
+}
+
+let control = new Control(controlList);
+
+
+
+
+
+
+
